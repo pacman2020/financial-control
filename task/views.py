f123bebe16955d06f7ccc9e3979f9d897f3f34da
geraldo <geeferreirasd034@gmail.com>
2020-10-23 21:42:00 -0300

    task listing

--- task/views.py
+++ task/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
 def tasks(request):
-    return render(request, 'tasks.html')
+    tasks = Task.objects.all()
+    data = {
+        'tasks': tasks
+    }
+
+    return render(request, 'task/list_task.html', data)
