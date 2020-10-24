05b57eb834a7b66a61c8158c82d9148cf25cc8e8
geraldo <geeferreirasd034@gmail.com>
2020-10-24 20:22:18 -0300

    implemented asset and price fields in the task model

--- task/models.py
+++ task/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    active = models.BooleanField(default=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
