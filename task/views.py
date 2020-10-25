470c6f9670fc77a9962d13b381ac1efba10cdf90
geraldo <geeferreirasd034@gmail.com>
2020-10-25 11:14:41 -0300

    task update function

--- task/views.py
+++ task/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
 from .models import Task
@@ -37,4 +38,20 @@ def new_task(request):
             return redirect('detail_task', pk=task.pk)
     else:
         form = Taskform()
-    return render(request, 'task/new_task.html', {'form':form})
+    return render(request, 'task/new_update_pet.html', {'form':form})
+
+def update_task(request, pk):
+    task = get_object_or_404(Task, pk=pk, user_id=request.user)
+
+    if request.method == 'POST':
+        form = Taskform(request.POST, instance=task)
+        if form.is_valid():
+            task.save()
+            return redirect('detail_task', pk=task.pk)
+    else:
+        form = Taskform(instance=task)
+        data = {
+            'form':form,
+            'task': task
+        }
+        return render(request, 'task/new_update_pet.html',data)
