bd8c418c608a9d8c9a8c3f4f4086f036cf576a5c
geraldo <geeferreirasd034@gmail.com>
2020-10-25 10:55:02 -0300

    form validation

--- task/forms.py
+++ task/forms.py
@@ -4,6 +4,9 @@ from django.forms import fields
 from .models import Task
 
 class Taskform(forms.ModelForm):
+    name = forms.CharField(label='nome da tarefa', max_length=100, required=True)
+    price = forms.DecimalField(label='preço da tarefa', required=True)
+    active = forms.BooleanField(label='tarefa ativa' ,required=True)
     class Meta:
         model = Task
         fields = ('name','price','active',)
