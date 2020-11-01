c2d22b2259cdc2ea05d92914086e7cd25bccba4c
geraldo <geeferreirasd034@gmail.com>
2020-11-01 08:27:05 -0300

    creating post table

--- financialControl/settings.py
+++ financialControl/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'task',
     'employee',
+    'post',
 ]
 
 MIDDLEWARE = [
--- post/models.py
+++ post/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from task.models import Task
+from employee.models import Employee
+
+class Post(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task_id.name
