9d6d07dded832bc2133caf32fae99b9bf422ba86
geraldo <geeferreirasd034@gmail.com>
2020-11-06 19:51:06 -0300

    company field corrections in task table form

--- financialControl/settings.py
+++ financialControl/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ge&&zm+_xqc6afstlxnve!redn%shfzq$9gfc00hh45m9&jbm6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
