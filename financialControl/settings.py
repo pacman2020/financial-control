7d4ccc7c0c8ec72d3e3ec948100147f005cbf031
geraldo <geeferreirasd034@gmail.com>
2020-11-08 08:28:45 -0300

    creating sales table

--- financialControl/settings.py
+++ financialControl/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'employee',
     'post',
     'product',
+    'sale',
 ]
 
 MIDDLEWARE = [
--- sale/models.py
+++ sale/models.py
@@ -1,3 +1,14 @@
+from product.models import Product
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Sale(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    purchase_price = models.DecimalField(max_digits=5, decimal_places=2)
+    amount = models.IntegerField(blank=False)
+    active = models.BooleanField(blank=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_id.name_product
