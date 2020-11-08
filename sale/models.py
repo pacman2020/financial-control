1e0edab00f9b1252b529e8f6fc36bd8b6cf02c1f
geraldo <geeferreirasd034@gmail.com>
2020-11-08 12:56:33 -0300

    removing the quantity field from the sales tables

--- sale/models.py
+++ sale/models.py
@@ -6,9 +6,8 @@ class Sale(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     product_id = models.ManyToManyField(Product)
     purchase_price = models.DecimalField(max_digits=5, decimal_places=2)
-    amount = models.IntegerField(blank=False)
     active = models.BooleanField(blank=False)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user_id
+        return self.user_id.username
