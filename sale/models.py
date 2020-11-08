9111ebacfeec5bd78cb216d7c0a7d2ca9b9ce0c9
geraldo <geeferreirasd034@gmail.com>
2020-11-08 12:24:52 -0300

    correction of the product_id field of the tables sale

--- sale/models.py
+++ sale/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 
 class Sale(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_id = models.ManyToManyField(Product)
     purchase_price = models.DecimalField(max_digits=5, decimal_places=2)
     amount = models.IntegerField(blank=False)
     active = models.BooleanField(blank=False)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.product_id.name_product
+        return self.user_id
